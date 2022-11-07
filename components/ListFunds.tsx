ea0d4a4ec5cb3f9923cbdd5bc172ccca738bf988
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-11-08 00:06:31 +0530

    Add loading indicator on ListFunds component

--- components/ListFunds.tsx
+++ components/ListFunds.tsx
@@ -1,25 +1,34 @@
 import { List, Text } from "@mantine/core";
+import { Loader } from "@mantine/core";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { Fund } from "../utils/types";
 import { getFund, getFundTokenAddresses } from "../utils/utils";
 
 export const ListFunds = () => {
+  const [loading, isLoading] = useState(false);
   const [funds, setFunds] = useState<Fund[] | null>(null);
 
   useEffect(() => {
     const fetchFunds = async () => {
+      isLoading(true);
       const fundTokenAddresses = await getFundTokenAddresses();
       const funds = await Promise.all(
         fundTokenAddresses.map(async (x) => await getFund(x))
       );
+      isLoading(false);
       setFunds(funds);
     };
 
     fetchFunds();
-  });
+  }, []);
 
-  return funds ? (
+  return loading ? (
+    <p>
+      {`Please wait `}
+      <Loader variant="dots" />
+    </p>
+  ) : funds ? (
     <List>
       {funds.map((fund) => (
         <List.Item key={fund.fundTokenAddress}>
