b33a2d5d0d71823c5e4b5329ef0b0532c9eecb02
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-11-10 23:55:45 +0530

    Fetch and display withdrawl requests of a fund - Display withdrawl requests details in a table

--- pages/funds/[fundTokenAddress]/index.tsx
+++ pages/funds/[fundTokenAddress]/index.tsx
@@ -9,17 +9,19 @@ import {
   Card,
   Badge,
   Title,
+  Table,
 } from "@mantine/core";
 import { IconCurrencyDollar } from "@tabler/icons";
 import { useRouter } from "next/router";
 import ErrorPage from "next/error";
 import Link from "next/link";
 
-import { Fund } from "../../../utils/types";
+import { Fund, WithdrawRequest } from "../../../utils/types";
 import {
   depositStableCoins,
   getFund,
   getFundReferences,
+  getWithdrawRequests,
 } from "../../../utils/utils";
 import { WalletContext } from "../../../utils/context";
 
@@ -37,7 +39,26 @@ const FundPage = () => {
   const [depositLoading, setDepositLoading] = useState(false);
   const [isFundManager, setIsFundManager] = useState(false);
   const [fundManagerAddress, setFundManagerAddress] = useState("");
+  const [withdrawRequests, setWithdrawRequests] = useState<
+    WithdrawRequest[] | null
+  >(null);
 
+  // const [rows, setRows] = useState<ReactElement[]>();
+  const elements = [
+    { position: 6, mass: 12.011, symbol: "C", name: "Carbon" },
+    { position: 7, mass: 14.007, symbol: "N", name: "Nitrogen" },
+    { position: 39, mass: 88.906, symbol: "Y", name: "Yttrium" },
+    { position: 56, mass: 137.33, symbol: "Ba", name: "Barium" },
+    { position: 58, mass: 140.12, symbol: "Ce", name: "Cerium" },
+  ];
+  const rows = elements.map((element) => (
+    <tr key={element.name}>
+      <td>{element.position}</td>
+      <td>{element.name}</td>
+      <td>{element.symbol}</td>
+      <td>{element.mass}</td>
+    </tr>
+  ));
   // Fetch fund details
   useEffect(() => {
     const fetchFund = async () => {
@@ -95,6 +116,25 @@ const FundPage = () => {
     setDepositLoading(false);
   };
 
+  // Fetch withdraw requests for a given fund
+  useEffect(() => {
+    const fetchWithdrawRequests = async () => {
+      if (typeof fundTokenAddress !== "string") return;
+
+      try {
+        const withdrawRequests = await getWithdrawRequests(fundTokenAddress);
+        if (!withdrawRequests) {
+          setIsFundTokenAddressValid(false);
+          return;
+        }
+        setWithdrawRequests(withdrawRequests);
+      } catch (error) {
+        setIsFundTokenAddressValid(false);
+      }
+    };
+    fetchWithdrawRequests();
+  }, [fundTokenAddress, connectedWallet]);
+
   // Show 404 if fund token address is invalid
   if (!isFundTokenAddressValid) return <ErrorPage statusCode={404}></ErrorPage>;
 
@@ -102,7 +142,7 @@ const FundPage = () => {
     <div>
       <LoadingOverlay visible={!fund} overlayBlur={2} />
 
-      <Card shadow="sm" p="lg" radius="md" mr={"30vw"} withBorder>
+      <Card shadow="sm" p="lg" radius="md" mr={"30vw"} mb={20} withBorder>
         <Group position="apart" mt="md" mb="xs">
           <Stack>
             <Title order={2}>{fund?.name}</Title>
@@ -170,6 +210,48 @@ const FundPage = () => {
             Create withdraw request
           </Button>
         ) : null}
+      </Card>
+
+      <Card shadow="sm" p="lg" radius="md" mr={"30vw"} withBorder>
+        <Group position="apart" mt="md" mb="xs">
+          <Stack>
+            <Title order={5}>{`Withdrawl requests`}</Title>
+            <Group>
+              <Table striped>
+                <thead>
+                  <tr>
+                    <th>No.</th>
+                    <th>Title</th>
+                    <th>Amount</th>
+                    <th>Status</th>
+                    <th>Votes for</th>
+                    <th>Votes against</th>
+                    <th>Deadline</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {withdrawRequests?.map((request, index) => (
+                    <tr key={index}>
+                      <td>{index + 1}</td>
+                      <td>{request.title}</td>
+                      <td>{request.amount}</td>
+                      <td>
+                        {request.status === 0
+                          ? "Open"
+                          : request.status === 1
+                          ? "Executed"
+                          : "Failed"}
+                      </td>
+                      <td>{request.numVotesFor}</td>
+                      <td>{request.numVotesAgainst}</td>
+                      <td>{request.deadline.toDateString()}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </Group>
+          </Stack>
+        </Group>
       </Card>
     </div>
   );
