74ae428fa996dbf0f54c3b640e1ee276751937da
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-11-12 17:54:04 +0530

    Remove unused code

--- pages/funds/[fundTokenAddress]/index.tsx
+++ pages/funds/[fundTokenAddress]/index.tsx
@@ -44,23 +44,6 @@ const FundPage = () => {
     WithdrawRequest[] | null
   >(null);
 
-  // const [rows, setRows] = useState<ReactElement[]>();
-  const elements = [
-    { position: 6, mass: 12.011, symbol: "C", name: "Carbon" },
-    { position: 7, mass: 14.007, symbol: "N", name: "Nitrogen" },
-    { position: 39, mass: 88.906, symbol: "Y", name: "Yttrium" },
-    { position: 56, mass: 137.33, symbol: "Ba", name: "Barium" },
-    { position: 58, mass: 140.12, symbol: "Ce", name: "Cerium" },
-  ];
-  const rows = elements.map((element) => (
-    <tr key={element.name}>
-      <td>{element.position}</td>
-      <td>{element.name}</td>
-      <td>{element.symbol}</td>
-      <td>{element.mass}</td>
-    </tr>
-  ));
-
   // Fetch fund details
   useEffect(() => {
     const fetchFund = async () => {
